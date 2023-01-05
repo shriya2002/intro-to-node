d4ce4aae510f261ee0f77be428fa759d83146e14
shriya2002 <shrivyamajala@gmail.com>
2023-01-05 14:40:54 -0700

    Commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var superheroes =require("superheroes");
+const supervillains = require("supervillains");
 var mySuperheroName =superheroes.random();
 console.log(mySuperheroName);
+
+var superheroes = require("supervillains");
+var mySupervillainName = supervillains.random();
+console.log(mySupervillainName);
